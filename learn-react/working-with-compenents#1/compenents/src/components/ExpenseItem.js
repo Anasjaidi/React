38643ebe839475d87edea7a2354e325cf8001efe
Anas jaidi <anas.jaidi@icloud.com>
2023-01-19 12:31:29 +0100

    first compenent

---
+++ learn-react/working-with-compenents#1/compenents/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = (_) => {
+	return (
+	<div>
+		<div>March 28th 2023</div>
+		<div>
+			<h2>Car Insurance</h2>
+			<div>$256.67</div>
+		</div>
+	</div>
+	);
+};
+
+export default ExpenseItem;
