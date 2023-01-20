d6108110ee024ba1467e81a8f15615adea19363f
Anas jaidi <anas.jaidi@icloud.com>
2023-01-20 16:59:35 +0100

    chore: re structure project folders

--- learn-react/working-with-compenents#1/src/App.js
+++ learn-react/working-with-compenents#1/src/App.js
@@ -1,12 +1,22 @@
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import React from "react";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
-	return (
-		<div className="App">
-			<Expenses/ >
-		</div>
+	// Jsx under the hood 
+	// 1) element string if it html elemnt
+	// 2) options like: classname or props
+	// 3) child element
+	return React.createElement(
+		"div",
+		{ className: "App" },
+		React.createElement(Expenses, {})
 	);
+	// return (
+	// 	<div className="App">
+	// 		<Expenses/ >
+	// 	</div>
+	// );
 }
 
 export default App;
