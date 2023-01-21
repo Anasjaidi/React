8ed1752d8c379e902267af5dee03b4eec14c0041
Anas jaidi <anas.jaidi@icloud.com>
2023-01-21 13:26:05 +0100

    chore: add newExpense compenent

--- learn-react/react-state-&-working-with-events#2/src/App.js
+++ learn-react/react-state-&-working-with-events#2/src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 	// Jsx under the hood 
 	// 1) element string if it html elemnt
 	// 2) options like: classname or props
 	// 3) child element
-	return React.createElement(
-		"div",
-		{ className: "App" },
-		React.createElement(Expenses, {})
-	);
-	// return (
-	// 	<div className="App">
-	// 		<Expenses/ >
-	// 	</div>
+	// return React.createElement(
+	// 	"div",
+	// 	{ className: "App" },
+	// 	React.createElement(Expenses, {})
 	// );
+	return (
+		<div className="App">
+			<NewExpense/>
+			<Expenses/ >
+		</div>
+	);
 }
 
 export default App;
