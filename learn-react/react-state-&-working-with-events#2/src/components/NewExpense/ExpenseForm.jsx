cb54b4bbd5fcb3effeb771ab26ce4bc106ccb635
Anas jaidi <anas.jaidi@icloud.com>
2023-01-21 13:35:05 +0100

    chore: add state to each input

--- learn-react/react-state-&-working-with-events#2/src/components/NewExpense/ExpenseForm.jsx
+++ learn-react/react-state-&-working-with-events#2/src/components/NewExpense/ExpenseForm.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import './ExpenseForm.css'
+import { useState } from "react";
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const titleChangeHandler = e => {
+    setTitle(e.target.value);
+  }
+  const amountChangeHandler = e => {
+    setTitle(e.target.value);
+  }
+  const dateChangeHandler = e => {
+    setTitle(e.target.value);
+  }
+
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" />
+					<input type="text" onChange={titleChangeHandler} value={title} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" />
+					<input type="number" onChange={amountChangeHandler} value={amount} />
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" />
+					<input type="date" onChange={dateChangeHandler} value={date} />
 				</div>
 			</div>
 			<div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
+				<button type="submit">Add Expense</button>
+			</div>
 		</form>
 	);
 };
