a45a31955680e45fb4bcf44f9c2dc5b8eef41fdc
Anas jaidi <anas.jaidi@icloud.com>
2023-01-22 13:41:41 +0100

    adding year filter statefull compenent

--- learn-react/react-state-&-working-with-events#2/src/components/Expenses/Expenses.jsx
+++ learn-react/react-state-&-working-with-events#2/src/components/Expenses/Expenses.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses() {
 	const expenses = [
@@ -9,9 +10,19 @@ export default function Expenses() {
 		{ title: "expense #2", amount: 352.45, date: new Date() },
 		{ title: "expense #3", amount: 987.68, date: new Date() },
 	];
+	const [filteredYear, setFilteredYear] = useState("2020");
+
+	const filterChangeHandler = (selectedYear) => {
+		console.log(selectedYear);
+		setFilteredYear(selectedYear);
+	};
 
 	return (
 		<Card className="expenses">
+			<ExpensesFilter
+				selected={filteredYear}
+				onChangeFilter={filterChangeHandler}
+			/>
 			{expenses.map((exp) => (
 				<ExpenseItem title={exp.title} amount={exp.amount} date={exp.date} />
 			))}
