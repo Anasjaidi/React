30d3addb91800d7df5b6ee737f95630749ed92e7
Anas jaidi <anas.jaidi@icloud.com>
2023-01-23 13:25:32 +0100

    add the key prop

--- learn-react/list-&conditional-rendering#3/src/App.js
+++ learn-react/list-&conditional-rendering#3/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+	const [expenses, setExpenses] = useState([
+		{ title: "expense #1", amount: 247.65, date: new Date(), id:"e1" },
+		{ title: "expense #2", amount: 352.45, date: new Date(), id:"e2" },
+		{ title: "expense #3", amount: 987.68, date: new Date(), id:"e3" },
+	]);
 	// Jsx under the hood 
 	// 1) element string if it html elemnt
 	// 2) options like: classname or props
@@ -15,11 +20,12 @@ function App() {
 	// );
 	const addExpenseHandler = expense => {
 		console.log(expense)
+		setExpenses(prevExpenses => [ expense, ...prevExpenses ])
 	}
 	return (
 		<div className="App">
 			<NewExpense onAddExpense={addExpenseHandler}/>
-			<Expenses/ >
+			<Expenses items={expenses} />
 		</div>
 	);
 }
--- learn-react/list-&conditional-rendering#3/src/components/Expenses/Expenses.jsx
+++ learn-react/list-&conditional-rendering#3/src/components/Expenses/Expenses.jsx
@@ -4,16 +4,10 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
-export default function Expenses() {
-	const expenses = [
-		{ title: "expense #1", amount: 247.65, date: new Date() },
-		{ title: "expense #2", amount: 352.45, date: new Date() },
-		{ title: "expense #3", amount: 987.68, date: new Date() },
-	];
+export default function Expenses(props) {
 	const [filteredYear, setFilteredYear] = useState("2020");
 
 	const filterChangeHandler = (selectedYear) => {
-		console.log(selectedYear);
 		setFilteredYear(selectedYear);
 	};
 
@@ -23,8 +17,8 @@ export default function Expenses() {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{expenses.map((exp) => (
-				<ExpenseItem title={exp.title} amount={exp.amount} date={exp.date} />
+			{props.items.map((exp) => (
+				<ExpenseItem title={exp.title} amount={exp.amount} date={exp.date} key={exp.id} />
 			))}
 		</Card>
 	);
