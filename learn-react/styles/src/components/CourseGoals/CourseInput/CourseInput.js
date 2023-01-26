0da6096228a83f5800428c896924d9aeb11ceca5
Anas jaidi <anas.jaidi@icloud.com>
2023-01-26 13:53:40 +0100

    adding dynamic class

--- learn-react/styles/src/components/CourseGoals/CourseInput/CourseInput.js
+++ learn-react/styles/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
-  const goalInputChangeHandler = event => {
-    if (event.target.value.trim().length > 0) setIsValid(true)
-    setEnteredValue(event.target.value);
-  };
+const CourseInput = (props) => {
+	const [enteredValue, setEnteredValue] = useState("");
+	const [isValid, setIsValid] = useState(true);
+	const goalInputChangeHandler = (event) => {
+		if (event.target.value.trim().length > 0) setIsValid(true);
+		setEnteredValue(event.target.value);
+	};
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    if (enteredValue.trim().length === 0) {
-      setIsValid(false); 
-      return ;
-    }
-    props.onAddGoal(enteredValue);
-    setEnteredValue('')
-  };
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			setIsValid(false);
+			return;
+		}
+		props.onAddGoal(enteredValue);
+		setEnteredValue("");
+	};
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: isValid ? 'black' : 'red'}}>Course Goal</label>
-        <input style={
-          {
-            borderColor: isValid ? 'black' : 'red',
-            background : isValid ? 'transparent' : 'salmon'
-          }
-          } type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className={"form-control ".concat(isValid ? "" : "invalid")}>
+				<label
+				>
+          {/* // style={{ color: isValid ? "black" : "red" }} */}
+					Course Goal
+				</label>
+				<input
+					// style={{
+					// 	borderColor: isValid ? "black" : "red",
+					// 	background: isValid ? "transparent" : "salmon",
+					// }}
+					type="text"
+					onChange={goalInputChangeHandler}
+					value={enteredValue}
+				/>
+			</div>
+			<Button type="submit">Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
