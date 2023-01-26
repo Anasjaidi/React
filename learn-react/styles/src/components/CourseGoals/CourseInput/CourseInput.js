0ffc24c6948dc7c989346e3c73fe465ac1b22bd5
Anas jaidi <anas.jaidi@icloud.com>
2023-01-26 16:20:25 +0100

    add props to styled-compenent

--- learn-react/styles/src/components/CourseGoals/CourseInput/CourseInput.js
+++ learn-react/styles/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormClass = styled.div`
+		margin: 0.5rem 0;
+
+	& label {
+		font-weight: bold;
+		display: block;
+		margin-bottom: 0.5rem;
+		color: ${props => props.valid ? 'black' : 'red'}
+	}
+
+	& input {
+		display: block;
+		width: 100%;
+		border: 1px solid ${props => props.valid ?'#ccc' : 'red'};
+		background-color : ${props => props.valid ? 'transparent' : 'red'}
+		font: inherit;
+		line-height: 1.5rem;
+		padding: 0 0.25rem;
+	}
+
+	& input:focus {
+		outline: none;
+		background: #fad0ec;
+		border-color: #8b005d;
+	}
+`;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
@@ -23,7 +51,7 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className={"form-control ".concat(isValid ? "" : "invalid")}>
+			<FormClass valid={isValid}>
 				<label
 				>
           {/* // style={{ color: isValid ? "black" : "red" }} */}
@@ -38,7 +66,7 @@ const CourseInput = (props) => {
 					onChange={goalInputChangeHandler}
 					value={enteredValue}
 				/>
-			</div>
+			</FormClass>
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
