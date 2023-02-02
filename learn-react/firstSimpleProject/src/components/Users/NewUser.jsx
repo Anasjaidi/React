4339781f5578dd230af75b247bc9d380436b9418
Anas jaidi <anas.jaidi@icloud.com>
2023-02-02 13:07:32 +0100

    add Error Modal

--- learn-react/firstSimpleProject/src/components/Users/NewUser.jsx
+++ learn-react/firstSimpleProject/src/components/Users/NewUser.jsx
@@ -1,40 +1,60 @@
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import { useState } from "react";
+import ErrorModal from "../UI/ErrorModal";
+import ErrorModalStyles from "../UI/ErrorModal.module.css";
 
-import styles from './NewUser.module.css'
+import styles from "./NewUser.module.css";
 
 const AddUser = (props) => {
+	const [entredUsername, setEntredUsername] = useState("");
+	const [entredaAge, setEntredaAge] = useState("");
 
-  const [entredUsername, setEntredUsername] = useState('');
-  const [entredaAge, setEntredaAge] = useState('');
-
-  const usernameChangeHandler = e => {
-    setEntredUsername(e.target.value)
-  }
-  const ageChangeHandler = e => {
-    setEntredaAge(e.target.value)
-  }
+	const usernameChangeHandler = (e) => {
+		setEntredUsername(e.target.value);
+	};
+	const ageChangeHandler = (e) => {
+		setEntredaAge(e.target.value);
+	};
 
 	const addNewUser = (e) => {
 		e.preventDefault();
-    if (entredaAge.trim().length === 0 || entredUsername.trim().length === 0) return
-    if (+entredaAge < 1) return;
-		props.addNewUser({name: entredUsername, age: entredaAge, id: Math.random().toString()})
-    setEntredUsername('')
-    setEntredaAge('')
+		if (entredaAge.trim().length === 0 || entredUsername.trim().length === 0)
+			return;
+		if (+entredaAge < 1) return;
+		props.addNewUser({
+			name: entredUsername,
+			age: entredaAge,
+			id: Math.random().toString(),
+		});
+		setEntredUsername("");
+		setEntredaAge("");
 	};
 
 	return (
-		<Card className={styles.input}>
-			<form onSubmit={addNewUser}>
-				<label htmlFor="username">username</label>
-				<input type="text" id="username" value={entredUsername} onChange={usernameChangeHandler}/>
-				<label htmlFor="age">age (years)</label>
-				<input type="number" id="age" value={entredaAge} onChange={ageChangeHandler}/>
-				<Button type="submit">add new user</Button>
-			</form>
-		</Card>
+		<div>
+			<div className={ErrorModalStyles.backdrop}></div>
+			<ErrorModal title="something" content="badly going wrong" />
+			<Card className={styles.input}>
+				<form onSubmit={addNewUser}>
+					<label htmlFor="username">username</label>
+					<input
+						type="text"
+						id="username"
+						value={entredUsername}
+						onChange={usernameChangeHandler}
+					/>
+					<label htmlFor="age">age (years)</label>
+					<input
+						type="number"
+						id="age"
+						value={entredaAge}
+						onChange={ageChangeHandler}
+					/>
+					<Button type="submit">add new user</Button>
+				</form>
+			</Card>
+		</div>
 	);
 };
 
