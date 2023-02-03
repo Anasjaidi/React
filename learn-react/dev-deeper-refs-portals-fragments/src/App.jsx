aec0032266018313067c0ac2bfc9bcc133f03de4
Anas jaidi <anas.jaidi@icloud.com>
2023-02-03 08:28:43 +0100

    add a wrapper class

---
+++ learn-react/dev-deeper-refs-portals-fragments/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Wrapper from './components/Helpers/Wrapper'
+
+function App() {
+  return (
+    <Wrapper className="App">
+      <h2>anas jaidi</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo iusto, recusandae, magnam, consequatur consequuntur cum magni assumenda esse repellendus doloremque aut placeat beatae provident repudiandae quasi iure explicabo sequi! Quod!</p>
+    </Wrapper>
+  )
+}
+
+export default App
---
+++ learn-react/dev-deeper-refs-portals-fragments/src/components/Helpers/Wrapper.jsx
@@ -0,0 +1,10 @@
+
+const Wrapper = props => {
+  console.log(props.children);
+  return (
+    props.children
+  )
+}
+
+
+export default Wrapper
