24fb80d1b8cbeb97dcd258f3fea6b229737d1ce4
Anas jaidi <anas.jaidi@icloud.com>
2023-02-03 08:34:43 +0100

    add fragments

--- learn-react/dev-deeper-refs-portals-fragments/src/App.jsx
+++ learn-react/dev-deeper-refs-portals-fragments/src/App.jsx
@@ -1,11 +1,20 @@
+import React from 'react'
 import './App.css'
 import Wrapper from './components/Helpers/Wrapper'
+import img1 from "./../../../../../Desktop/IMG20210510152317.jpg";
+import img2 from "./../../../../../Desktop/IMG20210510152324.jpg";
 
 function App() {
   return (
     <Wrapper className="App">
       <h2>anas jaidi</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo iusto, recusandae, magnam, consequatur consequuntur cum magni assumenda esse repellendus doloremque aut placeat beatae provident repudiandae quasi iure explicabo sequi! Quod!</p>
+    <React.Fragment>
+      <img src={img1} alt="" width="200"/>
+    </React.Fragment>
+    <>
+      <img src={img2} alt="" width="200"/>
+    </>
     </Wrapper>
   )
 }
