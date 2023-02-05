31df301a1bb7ac9d33a0a64717319ca125c6b241
Anas jaidi <anas.jaidi@icloud.com>
2023-02-05 12:55:54 +0100

    add consumer for the context after adding the provider

--- learn-react/advanced-side-effects-&-user-reducer-&-contextApi/src/components/MainHeader/Navigation.jsx
+++ learn-react/advanced-side-effects-&-user-reducer-&-contextApi/src/components/MainHeader/Navigation.jsx
@@ -2,28 +2,32 @@ import React from 'react';
 
 import classes from './Navigation.module.css';
 
+import ContextData from '../../context/ContextData';
+
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+		<ContextData.Consumer>
+			<nav className={classes.nav}>
+				<ul>
+					{props.isLoggedIn && (
+						<li>
+							<a href="/">Users</a>
+						</li>
+					)}
+					{props.isLoggedIn && (
+						<li>
+							<a href="/">Admin</a>
+						</li>
+					)}
+					{props.isLoggedIn && (
+						<li>
+							<button onClick={props.onLogout}>Logout</button>
+						</li>
+					)}
+				</ul>
+			</nav>
+		</ContextData.Consumer>
+	);
 };
 
 export default Navigation;
