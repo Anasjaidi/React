246296304ec4c50590b6db0e3c7d05e0f0c20c34
Anas jaidi <anas.jaidi@icloud.com>
2023-02-13 12:47:02 +0100

    link css to the modal and backdrop files

--- foodOrderApp/src/components/UI/Modal/Modal.jsx
+++ foodOrderApp/src/components/UI/Modal/Modal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "./Modal.module.css";
 
 export const Backdrop = (props) => {
-  return <div>{}</div>;
+  return <div className={styles.backdrop}>{}</div>;
 };
 export const Modal = (props) => {
-  return <div>{}</div>;
+  return <div className={styles.modal}>{}</div>;
 };
