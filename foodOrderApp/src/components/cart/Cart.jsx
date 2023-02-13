1dff47e70e6d9867719087e3c855a86d9c2c786b
Anas jaidi <anas.jaidi@icloud.com>
2023-02-13 12:32:06 +0100

    add cart component jsx

---
+++ foodOrderApp/src/components/cart/Cart.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      <div>
+        <span>Total Amount</span>
+        <span>39.17</span>
+      </div>
+      <div>
+        <button type="button">Close</button>
+        <button type="button">Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
