1e0d84d7854df2d2fcc27f0cb07cf1520aff4237
Anas jaidi <anas.jaidi@icloud.com>
2023-02-15 00:05:35 +0100

    creting first custom hookwq

--- learn-react/custom-hooks/src/hooks/useCounter.jsx
+++ learn-react/custom-hooks/src/hooks/useCounter.jsx
@@ -1,1 +1,14 @@
-export default () => {}
+import { useEffect, useState } from "react";
+
+export default () => {
+  const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter((prevCounter) => prevCounter - 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+}
