7e8e7d0c2d22da17762bc979123b789e85e09ac5
Anas jaidi <anas.jaidi@icloud.com>
2023-02-15 00:02:04 +0100

    add a custom hook

--- learn-react/custom-hooks/src/App.jsx
+++ learn-react/custom-hooks/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
-  return (
-    <div className='App'>
-      
-    </div>
-  )
+	return (
+		<React.Fragment>
+			<ForwardCounter />
+			<BackwardCounter />
+		</React.Fragment>
+	);
 }
 
-export default App
+export default App;
