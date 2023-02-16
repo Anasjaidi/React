64b181176916514032291a2433d6ef80dc87d9f6
Anas jaidi <anas.jaidi@icloud.com>
2023-02-16 22:47:28 +0100

    reset input value

--- learn-react/forms-and-inputs/src/components/SimpleInput.jsx
+++ learn-react/forms-and-inputs/src/components/SimpleInput.jsx
@@ -10,14 +10,20 @@ const SimpleInput = (props) => {
 
   const formSubmitHandler = e => {
     e.preventDefault();
+
     console.log(nameInput);
+
     console.log(nameInputRef.current.value)
+
+    // nameInputRef.current.value = "", is worked but not ideal 
+
+    setNameInput("") // for reset the input
   }
 	return (
 		<form onSubmit={formSubmitHandler}>
 			<div className="form-control">
 				<label htmlFor="name">Your Name</label>
-				<input type="text" id="name" ref={nameInputRef} onChange={nameInputOnChangeHandler}/>
+				<input type="text" id="name" ref={nameInputRef} onChange={nameInputOnChangeHandler} value={nameInput}/>
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
