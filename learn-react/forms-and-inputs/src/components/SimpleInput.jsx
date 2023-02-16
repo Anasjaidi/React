100a3a6b8ff80ce88f68d30ff7f1fe36acbf1c9f
Anas jaidi <anas.jaidi@icloud.com>
2023-02-16 22:43:06 +0100

    target input with two methods: ref, two binding way

--- learn-react/forms-and-inputs/src/components/SimpleInput.jsx
+++ learn-react/forms-and-inputs/src/components/SimpleInput.jsx
@@ -1,9 +1,23 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [nameInput, setNameInput] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputOnChangeHandler = e => {
+    setNameInput(e.target.value)
+  }
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    console.log(nameInput);
+    console.log(nameInputRef.current.value)
+  }
 	return (
-		<form>
+		<form onSubmit={formSubmitHandler}>
 			<div className="form-control">
 				<label htmlFor="name">Your Name</label>
-				<input type="text" id="name" />
+				<input type="text" id="name" ref={nameInputRef} onChange={nameInputOnChangeHandler}/>
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
