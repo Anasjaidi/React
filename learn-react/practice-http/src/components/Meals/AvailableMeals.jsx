03811255a45da235b1c909f174ffa64eaed95c65
Anas jaidi <anas.jaidi@icloud.com>
2023-02-21 11:24:58 +0100

    fetch meals from backend

--- learn-react/practice-http/src/components/Meals/AvailableMeals.jsx
+++ learn-react/practice-http/src/components/Meals/AvailableMeals.jsx
@@ -1,52 +1,71 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import {useEffect} from "react";
 
 const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'n',
+        description: '1',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+    useEffect(() => {
+        (async () => {
+            const res = await fetch("https://react-http-54af6-default-rtdb.firebaseio.com/meals.json");
+            const data = await res.json();
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+            const meals = []
+
+            for (let key in data) {
+                meals.push({
+                    id: key,
+                    name: data[key].name,
+                    price: data[key].price,
+                    description: data[key].description,
+                })
+            }
+          console.log(meals)
+        })()
+    }, [])
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
 };
 
 export default AvailableMeals;
