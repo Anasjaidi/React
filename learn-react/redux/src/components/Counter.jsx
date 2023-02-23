c8cd642a785d548949b474b71a654339299f8e12
Anas jaidi <anas.jaidi@icloud.com>
2023-02-23 11:57:58 +0100

    dispatch the state from component

--- learn-react/redux/src/components/Counter.jsx
+++ learn-react/redux/src/components/Counter.jsx
@@ -1,9 +1,17 @@
 import classes from './Counter.module.css';
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 const Counter = () => {
   const toggleCounterHandler = () => {};
+  const dispatch = useDispatch()
 
   const counter = useSelector(state => state.counter)
+
+    const incHandler = () => {
+      dispatch({type: "INC"})
+    }
+    const decHandler = () => {
+        dispatch({type: "DEC"})
+    }
 
   return (
     <main className={classes.counter}>
@@ -11,6 +19,10 @@ const Counter = () => {
       <div className={classes.value}>
           {counter}
       </div>
+        <div>
+            <button onClick={incHandler}>Inc</button>
+            <button onClick={decHandler}>Dec</button>
+        </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
