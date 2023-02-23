4812e83759b54abd40e5b75051179b468911a730
Anas jaidi <anas.jaidi@icloud.com>
2023-02-23 12:12:09 +0100

    dispatch the state with action payload

--- learn-react/redux/src/components/Counter.jsx
+++ learn-react/redux/src/components/Counter.jsx
@@ -13,6 +13,10 @@ const Counter = () => {
         dispatch({type: "DEC"})
     }
 
+    const addHandler = () => {
+      dispatch({type: "ADD", amount: 5})
+    }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
@@ -20,8 +24,10 @@ const Counter = () => {
           {counter}
       </div>
         <div>
-            <button onClick={incHandler}>Inc</button>
             <button onClick={decHandler}>Dec</button>
+            <button onClick={addHandler}>Add 5</button>
+            <button onClick={incHandler}>Inc</button>
+
         </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- learn-react/redux/src/store/store.js
+++ learn-react/redux/src/store/store.js
@@ -2,13 +2,17 @@ import {createStore} from 'redux'
 
 
 const counterReducer = (state = {counter : 0}, action) => {
-    if (action.type == "INC") {
+    if (action.type ==="INC") {
         return {
             counter: state.counter + 1
         }
-    } else if (action.type == "DEC") {
+    } else if (action.type === "DEC") {
         return {
             counter: state.counter - 1
+        }
+    } else if (action.type === "ADD") {
+        return {
+            counter: state.counter + action.amount
         }
     }
     return state
