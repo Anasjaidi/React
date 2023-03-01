59e4d1816c418a42f71b19b2c1d77e6dfaca8acd
Anas jaidi <anas.jaidi@icloud.com>
2023-03-01 11:38:13 +0100

    add social icons

--- blogApp/src/components/layouts/NavBar/NavBar.jsx
+++ blogApp/src/components/layouts/NavBar/NavBar.jsx
@@ -3,7 +3,12 @@ import styles from './NavBar.module.css'
 export default () => {
     return (
         <div className={styles.top}>
-            Hello, From NavBar!
+            <div>
+                <i className="fa-brands fa-facebook"></i>
+                <i className="fa-brands fa-twitter"></i>
+                <i className="fa-brands fa-instagram"></i>
+                <i className="fa-brands fa-pinterest"></i>
+            </div>
         </div>
     )
 }
