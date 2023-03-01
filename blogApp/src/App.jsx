79f993c9dde41ba646475ac6417d9e83b466265d
Anas jaidi <anas.jaidi@icloud.com>
2023-03-01 11:33:49 +0100

    add font awesome library

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import NavBar from "./components/layouts/NavBar/NavBar.jsx";
+import {Fragment} from "react";
 
 function App() {
 
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  )
+    return (
+        <Fragment>
+            <NavBar/>
+        </Fragment>
+    )
 }
 
 export default App
