aa06ca997ce61803e3456a58d11ee1d2c1d7a74b
Anas jaidi <anas.jaidi@icloud.com>
2023-03-03 12:21:08 +0100

    add logout haneler

--- learn-react/redux/src/components/Header.jsx
+++ learn-react/redux/src/components/Header.jsx
@@ -1,24 +1,33 @@
 import classes from './Header.module.css';
+import {useDispatch} from "react-redux";
+import {authActions} from "../store/store.js";
+import {useSelector} from "react-redux";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    const dispatch = useDispatch()
+    const isAuth = useSelector(state => state.auth.isAuth)
+    const logoutHandler = e => {
+        e.preventDefault();
+        dispatch(authActions.logout())
+    }
+    return (
+        <header className={classes.header}>
+            <h1>Redux Auth</h1>
+            {isAuth && (<nav>
+                <ul>
+                    <li>
+                        <a href='/'>My Products</a>
+                    </li>
+                    <li>
+                        <a href='/'>My Sales</a>
+                    </li>
+                    <li>
+                        <button onClick={logoutHandler}>Logout</button>
+                    </li>
+                </ul>
+            </nav>)}
+        </header>
+    );
 };
 
 export default Header;
