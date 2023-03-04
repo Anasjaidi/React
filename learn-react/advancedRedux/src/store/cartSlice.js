d9260a424d516d09c32dfa47cf0bf19fc9cfcc92
Anas jaidi <anas.jaidi@icloud.com>
2023-03-04 13:04:06 +0100

    start working on cart store slic

--- learn-react/advancedRedux/src/store/cartSlice.js
+++ learn-react/advancedRedux/src/store/cartSlice.js
@@ -1,12 +1,25 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const initialState = {}
+const initialState = {
+    items: [],
+    totalQuantity: 0
+}
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
+        addItem: (state, action) => {
+            const newItem = action.payload;
 
+            const exsitingItem = state.items.find(el => el.id === newItem.id)
+
+            if (!exsitingItem) {
+
+            } else {
+
+            }
+        }
     }
 })
 
