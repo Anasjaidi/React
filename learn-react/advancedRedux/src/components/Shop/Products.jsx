2f932573f50ba9b26f1aef9bd1bedfbad975233b
Anas jaidi <anas.jaidi@icloud.com>
2023-03-04 13:23:46 +0100

    render dummy products

--- learn-react/advancedRedux/src/components/Shop/Products.jsx
+++ learn-react/advancedRedux/src/components/Shop/Products.jsx
@@ -1,16 +1,29 @@
 import ProductItem from './ProductItem.jsx';
 import classes from './Products.module.css';
 
+
+const DUMMY_PRODUCTS = [
+    {id: 'i1', title: "product 1", price: 6, description: "my first product"},
+    {id: 'i2', title: "product 2", price: 6, description: "my second product"}
+]
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+          {
+              DUMMY_PRODUCTS.map(pr => {
+                  return (
+                      <ProductItem
+                          id={pr.id}
+                          key={pr.id}
+                          title={pr.title}
+                          price={pr.price}
+                          description={pr.description}
+                      />
+                  )
+              })
+          }
       </ul>
     </section>
   );
