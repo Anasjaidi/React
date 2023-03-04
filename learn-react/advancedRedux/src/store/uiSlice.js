2a8e53d05a5c10005cbce53b88c0a05d02a74d63
Anas jaidi <anas.jaidi@icloud.com>
2023-03-04 12:46:39 +0100

    finish UI store

--- learn-react/advancedRedux/src/store/uiSlice.js
+++ learn-react/advancedRedux/src/store/uiSlice.js
@@ -1,12 +1,14 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const initialState = {}
+const initialState = {cartIsVisible: false}
 
 const uiSlice = createSlice({
     name: "ui",
     initialState,
     reducers: {
-
+        toggle: (state) => {
+            state.cartIsVisible = !state.cartIsVisible
+        }
     }
 })
 
