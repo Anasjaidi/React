f7a0b0e05d4c02823e7d970c75671ab385a0003b
Anas jaidi <anas.jaidi@icloud.com>
2023-04-04 18:38:55 +0000

    add links

--- next/next-tuto/src/pages/news/list.js
+++ next/next-tuto/src/pages/news/list.js
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 const ListNews = () => {
     return (
-        <h1>List of news</h1>
+        <>
+            <h1>List of news</h1>
+            <ul>
+                <li><Link href={'/news/news1'}>news1</Link></li>
+                <li><Link href={'/news/news2'}>news2</Link></li>
+                <li><Link href={'/news/news3'}>news3</Link></li>
+            </ul>
+        </>
     );
 }
 
